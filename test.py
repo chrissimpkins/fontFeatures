f8647fa9c8663d358c27cb56308399e0623a1a4c
Chris Simpkins <git.simpkins@gmail.com>
2021-05-21 16:21:05 -0400

    new GPOS4 lookup check implementation

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ from fontTools.ttLib import TTFont
 import fontFeatures
 
 
+def is_encoded_glyph_available(tt, hex):
+    return hex in tt.getBestCmap()
+
+
 REQUIRED_BASE_YORUBA = [0x1ECD]
 REQUIRED_BASE_YORUBA_NAME = ["uni1ECD"]
 REQUIRED_MARK_YORUBA = [0x0300, 0x0301]
@@ -16,16 +20,30 @@ fontpath = Path("Montserrat-Regular.ttf")
 tt = TTFont(fontpath)
 ff = fontFeatures.ttLib.unparse(tt)
 
+# confirm base forms are available
+for hex in REQUIRED_BASE_YORUBA:
+    assert is_encoded_glyph_available(tt, hex)
 
-mark2base_routines_filtered = [
+print("Found all required base forms")
+
+# confirm mark forms are available
+for hex in REQUIRED_MARK_YORUBA:
+    assert is_encoded_glyph_available(tt, hex)
+
+print("Found all required mark forms")
+
+filtered_routines = [
     routine
     for routine in ff.routines
-    if routine.name.startswith("MarkToBase") or routine.name.startswith("MarkToMark")
+    if (REQUIRED_LANGS[0] in routine.languages)
+    or (REQUIRED_LANGS[1] in routine.languages)
+    or (REQUIRED_LANGS[2] in routine.languages)
 ]
 
-mark2base = [routine for routine in ff.routines if routine.lookupType == "MarkToBase"]
 
-import pprint
+mark2base = [
+    routine for routine in filtered_routines if routine.lookupType == "MarkToBase"
+]
 
 for routine in mark2base:
     for rule in routine.rules:
